6c71ad82c975d08ad9dcb628eacfa890387d2426
Aaliyan <muhammed.aaliyan101@gmail.com>
2021-03-03 19:44:58 -0500

    implemented search feature

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react'
 import Axios from 'axios';
 import {  Col, Card, Row } from 'antd';
 import ImageSlider from '../../utils/ImageSlider';
-import CheckBox from './Sections/CheckBox'
-import RadioBox from './Sections/RadioBox'
-import {continents, price} from './Sections/Data'
+import CheckBox from './Sections/CheckBox';
+import RadioBox from './Sections/RadioBox';
+import {continents, price} from './Sections/Data';
+import SearchFeature from './Sections/SearchFeature';
 
 
 const {Meta} = Card;
@@ -15,6 +16,7 @@ function LandingPage() {
     const [Skip, setSkip] = useState(0)
     const [Limit, setLimit] = useState(8)
     const [PostSize, setPostSize] = useState()
+    const [SearchTerms, setSearchTerms] = useState("")
     const [Filters, setFilters] = useState({
         continents : [],
         price : []
@@ -119,6 +121,22 @@ function LandingPage() {
         setFilters(newFilters)
     }
 
+    const updateSearchTerms = (newSearchTerm) => {
+        
+        const variables = {
+            skip: 0,
+            limit: Limit,
+            filters: Filters,
+            searchTerm: newSearchTerm
+        }
+        
+        setSkip(0)
+        setSearchTerms(newSearchTerm)
+
+        getProducts(variables)
+
+    }
+
     return (
         <div style={{ width: '75%', margin: '3rem auto' }}>
             <div style={{ textAlign: 'center' }}>
@@ -147,6 +165,14 @@ function LandingPage() {
             
 
             {/* Search  */}
+            <div style={{display: 'flex', justifyContent: 'flex-end', margin: '1rem auto'}}>
+                <SearchFeature 
+                    refreshFunction={updateSearchTerms}
+                />
+            </div>
+                
+
+
         
             {Products.length === 0 ?
                     <div style={{ display: 'flex', height: '300px', justifyContent: 'center', alignItems: 'center' }}>
