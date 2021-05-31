bcfecd0d823b1b723d412bc42fc9c1d3fe519d86
Aaliyan <muhammed.aaliyan101@gmail.com>
2021-05-31 18:57:33 -0400

    cloudinary fix

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     mongoURI:process.env.MONGO_URI,
-    CLOUD_NAME:process.env.CLOUD_NAME,
-    API_KEY:process.env.API_KEY,
-    API_SECRET:process.env.API_SECRET
+    cloudNAME:process.env.CLOUD_NAME,
+    apiKEY:process.env.API_KEY,
+    apiSECRET:process.env.API_SECRET
 }
--- server/routes/product.js
+++ server/routes/product.js
@@ -27,9 +27,9 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage }).single("file")
 
 cloudinary.config({
-    cloud_name: config.CLOUD_NAME,
-    api_key: config.API_KEY,
-    api_secret: config.API_SECRET
+    cloud_name: config.cloudNAME,
+    api_key: config.apiKEY,
+    api_secret: config.apiSECRET
 })
 
 
